fd69e7527db4a7aa6e57bf5b08a79d1a3e13bd9f
selene <seleneugs@gmail.com>
2022-05-05 13:43:27 -0500

    6) haciendo fallar la prueba del Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require('./../../lib/utils/Reader');
+
+describe("Test de Reader:", () => {
+    test("Leyendo el archivo", () => {
+        const reader = Reader.readJsonFile("visualpartners.json");
+        expect(reader.length).toBe(1);
+    });
+});
