937497daf1b52ab38b9b4fa8be7adf702e89c62c
selene <seleneugs@gmail.com>
2022-05-05 13:51:16 -0500

    8)Cambiando la version de jest para corregir error en GitHub Actions y corrigiendo el test de Reader

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,6 +3,6 @@ const Reader = require('./../../lib/utils/Reader');
 describe("Test de Reader:", () => {
     test("Leyendo el archivo", () => {
         const reader = Reader.readJsonFile("visualpartners.json");
-        expect(reader.length).toBe(1);
+        expect(reader.length).toBe(51);
     });
 });
