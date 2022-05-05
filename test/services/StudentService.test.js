86e2b49d2196cc7c696454f564db0bdd35b858d5
selene <seleneugs@gmail.com>
2022-05-05 15:22:17 -0500

    12)Modificando StudentService

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -3,17 +3,20 @@ const StudentService = require('./../../lib/services/StudentService');
 
 describe("Test de StudentService:", () => {
     test("1) Listando los estudiantes", () => {
-        const studentS = StudentService.getlistStudents();
-        expect(studentS.length).toBe(51);
+        const st = [{id:1,name:"lala",score:501,haveCertification:true},{id:2,name:"lele",score:499,haveCertification:true}];
+        const studentS = StudentService.getlistStudents(st);
+        expect(studentS.length).toBe(2);
     });
 
     test("2) Emails de estudiantes con certificado", () => {
-        const studentsWithCertification = StudentService.getEmailStudentsWCertification();
-        expect(studentsWithCertification.length).toBe(29);
+        const st = [{id:1,name:"lala",score:501,haveCertification:true},{id:2,name:"lele",score:499,haveCertification:true}];
+        const studentsWithCertification = StudentService.getEmailStudentsWCertification(st);
+        expect(studentsWithCertification.length).toBe(2);
     });
 
     test("3) Estudiantes con credits>500", () => {
-        const students500 = StudentService.getStudents500();
-        expect(students500.length).toBe(27);
+        const st = [{id:1,name:"lala",credits:501,haveCertification:true},{id:2,name:"lele",credits:499,haveCertification:true}];
+        const students500 = StudentService.getStudents500(st);
+        expect(students500.length).toBe(1);
     });
 });
