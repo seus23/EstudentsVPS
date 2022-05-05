cc25f00a65436908ee6c1ec748211cf87ac505e1
selene <seleneugs@gmail.com>
2022-05-05 17:13:50 -0500

    13) REFACTORIZANDO

---
+++ test/controller/StudentsController.test.js
@@ -0,0 +1,19 @@
+const StudentController = require('./../../lib/controller/StudentsController');
+
+
+describe("Test de StudentController:", () => {
+    test("1) Listando los estudiantes", () => {
+        const lista = StudentController.getListaEstudiantes();
+        expect(lista.length).toBe(51);
+    });
+
+    test("2) Emails de estudiantes con certificado", () => {
+        const emails = StudentController.getEmailStudentsWithCert();
+        expect(emails.length).toBe(29);
+    });
+
+    test("3) Estudiantes con credits>500", () => {
+        const credits = StudentController.getStudentsWCredits500();
+        expect(credits.length).toBe(27);
+    });
+});
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -2,20 +2,15 @@ const StudentService = require('./../../lib/services/StudentService');
 
 
 describe("Test de StudentService:", () => {
-    test("1) Listando los estudiantes", () => {
-        const st = [{id:1,name:"lala",score:501,haveCertification:true},{id:2,name:"lele",score:499,haveCertification:true}];
-        const studentS = StudentService.getlistStudents(st);
-        expect(studentS.length).toBe(2);
-    });
-
-    test("2) Emails de estudiantes con certificado", () => {
-        const st = [{id:1,name:"lala",score:501,haveCertification:true},{id:2,name:"lele",score:499,haveCertification:true}];
+    
+    test("1) Emails de estudiantes con certificado", () => {
+        const st = [{id:1,name:"lala",score:501,haveCertification:true,email:"lala@com"},{id:2,name:"lele",score:499,haveCertification:true,email:"lele@com"}];
         const studentsWithCertification = StudentService.getEmailStudentsWCertification(st);
         expect(studentsWithCertification.length).toBe(2);
     });
 
-    test("3) Estudiantes con credits>500", () => {
-        const st = [{id:1,name:"lala",credits:501,haveCertification:true},{id:2,name:"lele",credits:499,haveCertification:true}];
+    test("2) Estudiantes con credits>500", () => {
+        const st = [{credits:501,haveCertification:true,email:"lala@com"},{credits:499,haveCertification:true,email:"lele@com"}];
         const students500 = StudentService.getStudents500(st);
         expect(students500.length).toBe(1);
     });
