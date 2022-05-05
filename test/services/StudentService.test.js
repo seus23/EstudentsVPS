d08f4611aa5307dd38f85cd17c0da0b34c17474f
selene <seleneugs@gmail.com>
2022-05-05 14:25:22 -0500

    10)Agregando prueba 2 a StudentService y 2do método static necesario para pasar el test

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -2,8 +2,13 @@ const StudentService = require('./../../lib/services/StudentService');
 
 
 describe("Test de StudentService:", () => {
-    test("Listando los estudiantes", () => {
+    test("1) Listando los estudiantes", () => {
         const studentS = StudentService.getlistStudents();
         expect(studentS.length).toBe(51);
+    });
+
+    test("2) Emails de estudiantes con certificado", () => {
+        const studentsWithCertification = StudentService.getEmailStudentsWCertification();
+        expect(studentsWithCertification.length).toBe(29);
     });
 });
